0c74f5309178b24c411a60e2901e585bad616f8f
Deelan Kilel <kileldylan@gmail.com>
2025-02-12 12:19:32 +0300

    COnnected it with the admin panel and also customized it

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD' : '9923',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
+
 
 
 # Password validation
