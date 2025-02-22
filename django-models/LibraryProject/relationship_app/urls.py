75368103a7f918abcfffbe89b9de7be6a1bca425
kileldylan <kileldylan@gmail.com>
2025-02-22 21:31:47 +0300

    intial commit

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from relationship_app.views import home,list_books, LibraryDetailView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", home, name="home"),  # Home route
+
+    # Function-based view (FBV)
+    path('books/', list_books, name='/relationship_app/list_books'),
+
+    # Class-based view (CBV) requires `as_view()`
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='/relationship_app/library_detail'),
+]
