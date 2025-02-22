1b60f9a8cba16c411cd93f4fe971865ebb7a05eb
kileldylan <kileldylan@gmail.com>
2025-02-22 13:59:03 +0300

    fixed the sample query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,10 +10,10 @@ def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)  # Match expected query format
     return library.books.all()  # ManyToManyField relation
 
-# Retrieve the librarian for a library
+# Retrieve the librarian for a library using the expected format
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)  # Match expected query format
-    return library.librarian  # OneToOneField relation
+    library = Library.objects.get(name=library_name)  # Get the library instance
+    return Librarian.objects.get(library=library)  # Explicitly query Librarian
 
 # Sample test run (uncomment if testing in Django shell)
 # print(get_books_by_author("J.K. Rowling"))
