d7969ac456642d96511f371e833a342e2711ecf7
kileldylan <kileldylan@gmail.com>
2025-02-23 20:42:58 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import home, list_books, admin_view, librarian_view, member_view, LibraryDetailView, CustomLoginView
-from .views import register
+from . import views
+from .views import LogoutView
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
-    path("login/", CustomLoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("", views.home, name="home"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),  # Now properly referenced
+    path("login/", views.CustomLoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
-    path("admin_view/", admin_view, name="admin_view"),
-    path("librarian_view/", librarian_view, name="librarian_view"),
-    path("member_view/", member_view, name="member_view"),
+    path("admin_view/", views.admin_view, name="admin_view"),
+    path("librarian_view/", views.librarian_view, name="librarian_view"),
+    path("member_view/", views.member_view, name="member_view"),
 ]
+
