207d6e5975b53146a15226da330a19f48bd5ecb6
kileldylan <kileldylan@gmail.com>
2025-02-23 22:40:15 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
+from .views import list_books
 
 def home(request):
     return redirect("list_books") 
