52ce44e0578d8f5d7035ab39c846a7f9077a3ceb
kileldylan <kileldylan@gmail.com>
2025-02-23 20:28:35 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import home, list_books, register, admin_view, librarian_view, member_view, LibraryDetailView
+from django.contrib.auth.views import LogoutView
+from .views import home, list_books, register_view, admin_view, librarian_view, member_view, LibraryDetailView, CustomLoginView
 
 
 urlpatterns = [
     path("", home, name="home"),
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
-    
-    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("register/", register_view, name="register"),
+    path("login/", CustomLoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
-
-    # Corrected paths to match the expected view file structure
     path("admin_view/", admin_view, name="admin_view"),
     path("librarian_view/", librarian_view, name="librarian_view"),
     path("member_view/", member_view, name="member_view"),
