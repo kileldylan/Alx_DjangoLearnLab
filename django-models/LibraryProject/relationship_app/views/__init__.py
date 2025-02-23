d79e89b28b8754d943ff0d14e6bc8cb48b4b8cbb
kileldylan <kileldylan@gmail.com>
2025-02-23 20:36:11 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/views/__init__.py
+++ django-models/LibraryProject/relationship_app/views/__init__.py
@@ -4,4 +4,4 @@ from .librarian_view import librarian_view
 from .member_view import member_view
 from .library_detail_view import LibraryDetailView
 from .custom_login import CustomLoginView
-from .register_view import register_view
+from .register_view import register
