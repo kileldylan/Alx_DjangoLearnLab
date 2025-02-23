75552d7babd07966b3124a98bb2a87c95211aa11
kileldylan <kileldylan@gmail.com>
2025-02-23 10:15:03 +0300

    initial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout  
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView
+from django.views.generic import CreateView, DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Library, Book  
 
@@ -29,7 +29,6 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
-   
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")  # Redirect to login after signup
@@ -40,22 +39,3 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = "logout.html"
-
-'''
-def user_login(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("home")
-        else:
-            form = AuthenticationForm()
-            return render(request, "relationship_app/login.html", {"form": form})
-
-def user_logout(request):
-    if request.method == "POST":
-        logout(request)
-        return redirect("home")
-    
-'''
