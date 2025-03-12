75aeddd1550f17d315fcc9b5b6cad7cfe358cecf
kileldylan <kileldylan@gmail.com>
2025-03-12 14:32:14 +0300

    intial commit

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -5,10 +5,20 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
-
+    
+    def validate(self, data):
+        if len(data['title']) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long.")
+        return data
+    
 class AuthorSerializer(serializers.ModelSerializer):
     name = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
         fields = "__all__"
+
+    def validate(self, data):
+        if len(data['name']) < 5:
+            raise serializers.ValidationError("Name must start with a caps.")
+        return data
