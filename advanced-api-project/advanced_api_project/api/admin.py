b41b6c92b7731189e8a4d38bcd09da3ccbdbc7be
kileldylan <kileldylan@gmail.com>
2025-03-12 13:09:58 +0300

    initiall commit

--- advanced-api-project/advanced_api_project/api/admin.py
+++ advanced-api-project/advanced_api_project/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Books, Author
 
 # Register your models here.
+@admin.register(Books)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+
+# Author Admin
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
