d9bd5d87ee1e494a3176fe34550e99eabc6c7f8a
kileldylan <kileldylan@gmail.com>
2025-03-13 08:28:03 +0300

    custom views, and generic views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailUpdateDeleteView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookDetailUpdateDeleteView.as_view(), name='book-detail-update-delete'),
+    path('book/', BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('book/create/', BookCreateView.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
