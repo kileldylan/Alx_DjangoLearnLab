835ba96a1a6fd6cd775b40545cce8c519b6e7316
kileldylan <kileldylan@gmail.com>
2025-03-13 08:51:14 +0300

    updated the generic class based views to have the necessary permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,37 +1,46 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django.http import HttpResponseForbidden
+from rest_framework.permissions import IsAuthenticatedOrReadOnly 
 from .models import Book
-from .serializers import BookSerializer
 
-# ListView (Public Read, Authenticated Users Can Create)
 class BookListView(ListView):
     model = Book
     template_name = 'book_list.html'
     context_object_name = 'books'
 
-# DetailView (Public Read)
 class BookDetailView(DetailView):
     model = Book
     template_name = 'book_detail.html'
 
-# CreateView (Only Authenticated Users)
-class BookCreateView(LoginRequiredMixin, CreateView):
+class BookCreateView(CreateView):
     model = Book
     template_name = 'book_create.html'
-    fields = ['title', 'publication_year' , 'author']
-    login_url = '/login/'  # Redirect to login page if unauthenticated
+    fields = ['title', 'author', 'description', 'published_date']
 
-# UpdateView (Only Authenticated Users)
-class BookUpdateView(LoginRequiredMixin, UpdateView):
+    def dispatch(self, request, *args, **kwargs):
+        permission = IsAuthenticatedOrReadOnly()
+        if not permission.has_permission(request, self):
+            return HttpResponseForbidden("You don't have permission to create a book.")
+        return super().dispatch(request, *args, **kwargs)
+
+class BookUpdateView(UpdateView):
     model = Book
     template_name = 'book_update.html'
-    fields = ['title', 'publication_year' , 'author']
-    login_url = '/login/'
+    fields = ['title', 'author', 'description', 'published_date']
 
-# DeleteView (Only Authenticated Users)
-class BookDeleteView(LoginRequiredMixin, DeleteView):
+    def dispatch(self, request, *args, **kwargs):
+        permission = IsAuthenticatedOrReadOnly()
+        if not permission.has_permission(request, self):
+            return HttpResponseForbidden("You don't have permission to update this book.")
+        return super().dispatch(request, *args, **kwargs)
+
+class BookDeleteView(DeleteView):
     model = Book
     template_name = 'book_delete.html'
-    success_url = '/'  # Redirect after deleting
-    login_url = '/login/'
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        permission = IsAuthenticatedOrReadOnly()
+        if not permission.has_permission(request, self):
+            return HttpResponseForbidden("You don't have permission to delete this book.")
+        return super().dispatch(request, *args, **kwargs)
