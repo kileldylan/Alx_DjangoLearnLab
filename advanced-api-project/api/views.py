0d41b6cdbec25c93fee780dba9fb1cd194fd557b
kileldylan <kileldylan@gmail.com>
2025-03-13 08:23:53 +0300

    custom views, and generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from  .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+#view to list(get) and create(post) books
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #Read for all, write for authenticated users
+
+#view to update books
+# DetailView: Handles GET (retrieve)
+# UpdateView: Handles PUT (update)
+# DeleteView: Handles DELETE
+class BookDetailUpdateUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] #restricts updates and deletes
