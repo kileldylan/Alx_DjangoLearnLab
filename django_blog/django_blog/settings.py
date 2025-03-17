4d8a2e4a50190f0301b58770576d072f2124693c
kileldylan <kileldylan@gmail.com>
2025-03-17 17:06:22 +0300

    configured the database settings to include user and port

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -72,8 +72,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PORT' : '3306',
+        'PASSWORD': '1609@Kilel',
     }
 }
 
