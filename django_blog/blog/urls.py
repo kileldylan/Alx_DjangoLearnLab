679c4663e395e26e3e97d069d7ac80731bb05f04
kileldylan <kileldylan@gmail.com>
2025-03-18 09:41:30 +0300

    created templates and rendered them based on permission and authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view, template_name=('blog/login.html'), name = 'login'),
-    path('logout/', views.custom_logout, name='logout'),
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name = 'login'),
+    path('accounts/logout/', views.custom_logout, name='logout'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('posts/', views.BlogListView.as_view(), name = 'post-list'),
+    path('posts/<int:pk>/', views.BlogDetailView.as_view(), name = 'post-detail'),
+    path('posts/new', views.BlogCreateView.as_view(), name = 'post-create'),
+    path('posts/<int:pk>/edit/', views.BlogUpdateView.as_view, name = 'post-edit'),
+    path('posts/<int:pk>/delete/', views.BlogDeleteView.as_view(), name = 'post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect, render
 from .forms import CustomUserCreateForm
 from django.contrib.auth import login,logout
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
+from .models import Post
+from django.http import HttpResponseForbidden
+from .forms import PostForm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import user_passes_test
 
 #view to handle user registration and login
 def register(request):
@@ -29,3 +36,52 @@ def profile(request):
 def custom_logout(request):
     logout(request)
     return redirect('login')
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'books'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/blog_detail.html'
+
+def is_admin(user):
+    return user.is_staff
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/blog_create_edit.html'
+    context_object_name = 'post-create'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set logged-in user as author
+        return super().form_valid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("You must be logged in to perform this action.")
+        if not request.user.is_superuser:  # or any other role condition
+            return HttpResponseForbidden("You do not have permission to perform this operation!")
+        return super().dispatch(request, *args, **kwargs)
+    
+@method_decorator(user_passes_test(is_admin), name='dispatch')
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/blog_create_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("You don't have permissions to Update!")
+        return super().dispatch(request, *args, **kwargs)
+    
+@method_decorator(user_passes_test(is_admin), name='dispatch')  
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/blog_delete.html'
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("You don't have permission to Delete")
+        return super().dispatch(request, *args, **kwargs)
+
