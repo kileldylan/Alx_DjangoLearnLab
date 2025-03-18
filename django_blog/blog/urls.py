0133af09d342e44cc5d0c0e60ed9722b3c50b548
kileldylan <kileldylan@gmail.com>
2025-03-18 09:44:44 +0300

    created templates and rendered them based on permission and authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('accounts/profile/', views.profile, name='profile'),
     path('post/', views.BlogListView.as_view(), name = 'post-list'),
     path('post/<int:pk>/', views.BlogDetailView.as_view(), name = 'post-detail'),
-    path('post/new', views.BlogCreateView.as_view(), name = 'post-create'),
-    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view, name = 'post-edit'),
+    path('post/new/', views.BlogCreateView.as_view(), name = 'post-create'),
+    path('post/<int:pk>/update/', views.BlogUpdateView.as_view, name = 'post-edit'),
     path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name = 'post-delete'),
 ]
