8d2b11dd0c8fd55b70977f1ff596648623a35b3b
kileldylan <kileldylan@gmail.com>
2025-03-20 18:37:14 +0300

    added the postbytaglistview and included its url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', views.search_results, name='search-posts'),
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
-    path('tags_slug/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 ]
